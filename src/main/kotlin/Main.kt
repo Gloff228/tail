c40779b965586e7a9963933ade2109d71b049769
Gloff228 <romo4ka101@gmail.com>
2022-04-05 11:08:50 +0300

    fixes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,22 @@
 import kotlinx.cli.*
 import java.io.File
+import java.io.FileNotFoundException
 
 fun main(args: Array<String>) {
+    parsing(args)
+}
+
+fun parsing(args: Array<String>) {
     val parser = ArgParser("tail")
     val extractSymbols by parser.option(
         ArgType.Int,
         shortName = "c",
-        description = "Number of symbols for output. Cannot be used with -n"
+        description = "Number of symbols for output. Can't be used with -n"
     )
     val extractStrings by parser.option(
         ArgType.Int,
         shortName = "n",
-        description = "Number of strings for output. Cannot be used with -c"
+        description = "Number of strings for output. Can't be used with -c"
     )
     val output by parser.option(ArgType.String, shortName = "o", fullName = "out", description = "Output file")
         .default("console")
@@ -21,65 +26,49 @@ fun main(args: Array<String>) {
 
     when {
         extractSymbols != null && extractStrings != null -> throw IllegalArgumentException("Can't use both -c and -n at the same time")
-        extractSymbols == null && extractStrings == null -> extractingStrings(input, output, 10)
-        extractSymbols != null -> extractingSymbols(input, output, extractSymbols!!)
-        extractStrings != null -> extractingStrings(input, output, extractStrings!!)
+        extractSymbols == null && extractStrings == null -> extracting(input, output, 10, null)
+        else -> extracting(input, output, extractStrings, extractSymbols)
     }
 }
 
-fun extractingSymbols(inputFile: List<String>, outputFile: String, count: Int) {
-    val list = mutableListOf<String>()
-    var isFile = true
-    for (file in inputFile) {
-        var lines: List<String>
+fun extracting(inputFile: List<String>, outputFile: String, countStrings: Int?, countSymbols: Int?) {
+    val list = mutableListOf<String>() //собирает строки, которые будут выводиться в output файл
 
-        if (file.substring(file.length - 4, file.length) != ".txt"){
-            isFile = false
-            lines = file.split("\\n")
-            if (inputFile.size > 1) list.add("---cmd String---")
-        } else {
-            if (!isFile) throw IllegalArgumentException("Can't be txt files and cmd text at the same time")
-            if (inputFile.size > 1) list.add("---$file---")
-            lines = File(file).readLines()
+    // проверка, является ли 1 элемент текстовым файлом (если нет, то последующие входные данные принимаются за простой текст)
+    if (inputFile[0].length < 4 || inputFile[0].substring(inputFile[0].length - 4, inputFile[0].length) != ".txt") {
+        val lines = inputFile.joinToString(separator = " ").split("\\n") //разделение текста cmd на строки
+        extractingSymbolOrString(lines, list, countSymbols, countStrings)
+    } else
+        for (file in inputFile) {
+            if (!File(file).exists()) throw FileNotFoundException("Called file does not exist") //исключение, если файла не существует
+            val lines = File(file).readLines() //разделение текста файла на строки
+            if (inputFile.size > 1) list.add("---$file---") //добавление названия файла при выводе, если файлов несколько
+            extractingSymbolOrString(lines, list, countSymbols, countStrings)
         }
-
-        var string = ""
-        for (line in lines.reversed()) {
-            string = line + string
-            if (string.length >= count) {
-                string = string.substring(string.length - count, string.length)
-                break
-            }
-        }
-        list.add(string)
-    }
     output(list, outputFile)
 }
 
-fun extractingStrings(inputFile: List<String>, outputFile: String, count: Int) {
-    val list = mutableListOf<String>()
-    var isFile = true
-    for (file in inputFile) {
-        var lines: List<String>
+fun extractingSymbolOrString(lines: List<String>, list: MutableList<String>, countSymbols: Int?, countStrings: Int?){
+    if (countStrings != null) extractingStrings(lines, list, countStrings)
+    else extractingSymbols(lines, list, countSymbols!!)
+}
 
-        if (file.substring(file.length - 4, file.length) != ".txt"){
-            isFile = false
-            lines = file.split("\\n")
-            if (inputFile.size > 1) list.add("---cmd String---")
-        } else {
-            if (!isFile) throw IllegalArgumentException("Can't be txt files and cmd text at the same time")
-            if (inputFile.size > 1) list.add("---$file---")
-            lines = File(file).readLines()
-        }
+fun extractingStrings(lines: List<String>, list: MutableList<String>, count: Int) {
+    var countForFile = count
+    if (count >= lines.size) countForFile = lines.size //сокращение выводимых строк до количества строк в файле
+    for (line in (lines.size - countForFile) until lines.size) list.add(lines[line])
+}
 
-        var countForFile = count
-        val strings =lines
-        if (count >= strings.size) countForFile = strings.size
-        for (line in (strings.size - countForFile) until strings.size) {
-            list.add(strings[line])
+fun extractingSymbols(lines: List<String>, list: MutableList<String>, count: Int) {
+    var string = ""
+    for (line in lines.reversed()) {
+        string = line + string
+        if (string.length >= count) {
+            string = string.substring(string.length - count, string.length)
+            break
         }
     }
-    output(list, outputFile)
+    list.add(string)
 }
 
 fun output(list: List<String>, outputFile: String) {
